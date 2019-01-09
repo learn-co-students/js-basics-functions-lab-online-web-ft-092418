86b90f87fe9f822ae37e1e34615490f35021c13c
Peter-G-Stone <peter.stone.online@gmail.com>
2019-01-09 17:50:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(st){
+    let ans = st - 42;
+    if (ans < 0){
+        ans = -ans;
+    }
+    return ans;
+}
+
+function distanceFromHqInFeet(st){
+   return distanceFromHqInBlocks(st) * 264;
+}
+
+function distanceTravelledInFeet(st, fin){
+    let blocks = fin - st; 
+    blocks < 0 ? blocks = -blocks : null;
+    return blocks * 264;
+
+ }
+ 
+ function calculatesFarePrice(start, destination){
+    ft = distanceTravelledInFeet(start, destination)
+    let fare = 0;
+    switch (true){
+
+        case ft >= 400 && ft <= 2000:
+            fare = (ft - 400) * .02;
+            break;
+        case ft > 2000 && ft <= 2500:
+            fare = 25;
+            break;
+        case ft > 2500:
+            fare = 'cannot travel that far';
+    }
+    return fare;
+ }
